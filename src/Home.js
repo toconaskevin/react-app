d142b7e3abc0dfce85c32b19966e5d6b079f796c
Kevin T <toconaskevin@gmail.com>
2019-10-25 10:05:13 -0300

    Carousel component added

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Carousel } from './components/Carousel';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap'
 
 export const Home = () => (
     <div>
@@ -16,55 +18,7 @@ export const Home = () => (
             <br/>
             <p>Popular MYtineraries</p>
 
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                
-                <div class="carousel-inner">
-
-                    <div class="carousel-item active">
-                        <div class="container">
-                            <div class="row text-center text-lg-left">
-                                <div class="col-lg-6 col-md-4 col-6">
-                                    <a href="#" class="d-block mb-4 h-100">
-                                    <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="" />
-                                    </a>
-                                </div>
-                                <div class="col-lg-6 col-md-4 col-6">
-                                    <a href="#" class="d-block mb-4 h-100">
-                                    <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="" />
-                                    </a>
-                                </div>
-                                <div class="col-lg-6 col-md-4 col-6">
-                                    <a href="#" class="d-block mb-4 h-100">
-                                    <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="" />
-                                    </a>
-                                </div>
-                                <div class="col-lg-6 col-md-4 col-6">
-                                    <a href="#" class="d-block mb-4 h-100">
-                                    <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-
-            </div>
+            <Carousel/>
 
         </body>
     </div>
